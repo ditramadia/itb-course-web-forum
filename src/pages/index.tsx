8cc473cbc607a0adfd41e3a76f92ea62144e2c22
ditramadia <ditrarizqaamadia@gmail.com>
2022-08-09 14:02:35 +0700

    update navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import Navbar from "./components/organism/Navbar";
+import Navbar from "../components/organism/Navbar";
 
 const Home: NextPage = () => {
   return (
