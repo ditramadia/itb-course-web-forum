d3f5c9499438eaad0abd797d7a06f1a69fc0ab17
ditramadia <ditrarizqaamadia@gmail.com>
2022-08-09 19:22:30 +0700

    update search

--- src/src/components/organism/Search/index.tsx
+++ src/src/components/organism/Search/index.tsx
@@ -10,7 +10,17 @@ export default function Search() {
             type="text"
             placeholder="Masukkan kata kunci"
           />
-          <input id="major-input" type="text" placeholder="Jurusan" />
+          {/* <input id="major-input" type="text" placeholder="Jurusan" /> */}
+          <select name="major" id="major-input" placeholder="Jurusan">
+            <option value="Teknik Informatika">Teknik Informatika</option>
+            <option value="Sistem Teknologi Informasi">
+              Sistem Teknologi Informasi
+            </option>
+            <option value="Teknik Elektro">Teknik Elektro</option>
+            <option value="Teknik Tenaga Listrik">Teknik Tenaga Listrik</option>
+            <option value="Teknik Telekomunikasi">Teknik Telekomunikasi</option>
+            <option value="Teknik Biomedis">Teknik Biomedis</option>
+          </select>
           <select name="category" id="category-input" placeholder="Kategori">
             <option value="Semester 1">Semua</option>
             <option value="Semester 1">Matkul Wajib</option>
