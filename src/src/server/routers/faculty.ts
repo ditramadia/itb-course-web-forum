6de2b8965a4060a449bb1b9ee808ea9ddff6ca47
Akbar Maulana R <akbarmaulanaridho1@gmail.com>
2022-08-09 18:25:48 +0700

    major and faculty model router

---
+++ src/src/server/routers/faculty.ts
@@ -0,0 +1,49 @@
+import * as trpc from '@trpc/server'
+import { Context } from '../context'
+import { z } from 'zod'
+
+export const facultyRouter = trpc
+  .router<Context>()
+  .query('findAll', {
+    resolve: async ({ ctx }) => {
+      return await ctx.prisma.faculty.findMany()
+    },
+  })
+  .mutation('insertOne', {
+    input: z.object({
+      name: z.string(),
+      acronym: z.string(),
+    }),
+    resolve: async ({ input, ctx }) => {
+      return await ctx.prisma.faculty.create({
+        data: input,
+      })
+    },
+  })
+  .mutation('updateOne', {
+    input: z.object({
+      id: z.number(),
+      name: z.string(),
+      acronym: z.string(),
+    }),
+    resolve: async ({ input, ctx }) => {
+      const { id, ...rest } = input
+
+      return await ctx.prisma.faculty.update({
+        where: { id },
+        data: rest,
+      })
+    },
+  })
+  .mutation('deleteOne', {
+    input: z.object({
+      id: z.number(),
+    }),
+    resolve: async ({ input, ctx }) => {
+      return await ctx.prisma.faculty.delete({
+        where: {
+          id: input.id,
+        },
+      })
+    },
+  })
