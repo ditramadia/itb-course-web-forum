e6e6c63fc5b7d420ef81c71ba4e57a9c9acc6fc9
ditramadia <ditrarizqaamadia@gmail.com>
2022-08-16 19:22:30 +0700

    update help page

--- src/src/components/organism/FaqContent/index.tsx
+++ src/src/components/organism/FaqContent/index.tsx
@@ -3,36 +3,35 @@ import React from 'react'
 export default function FaqContent() {
   return (
     <div className="accordion">
-        <div className="accordion-item" id='question1'>
-          <a className='accordion-link' href='#question1'>
-            Bagaimana cara menambah mata kuliah?
-          </a>
-          <div className="answer">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel possimus iusto aperiam, rerum nam tenetur, voluptatibus pariatur nulla commodi autem, necessitatibus deserunt optio fugiat sequi atque itaque? Dolorum, rem explicabo!
-            </p>
-          </div>
+      <div className="accordion-item" id="question1">
+        <a className="accordion-link" href="#question1">
+          Saya tidak menemukan mata kuliah yang dicari
+        </a>
+        <div className="answer">
+          <p>
+            Periksa kembali. Apabila tetap tidak ada, kontak kami dan akan kami
+            beri solusinya
+          </p>
         </div>
-        <div className="accordion-item" id='question2'>
-          <a className='accordion-link' href='#question2'>
-            Bagaimana cara menambah mata kuliah?
-          </a>
-          <div className="answer">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel possimus iusto aperiam, rerum nam tenetur, voluptatibus pariatur nulla commodi autem, necessitatibus deserunt optio fugiat sequi atque itaque? Dolorum, rem explicabo!
-            </p>
-          </div>
+      </div>
+      <div className="accordion-item" id="question2">
+        <a className="accordion-link" href="#question2">
+          Bagaimana cara menambah feedback?
+        </a>
+        <div className="answer">
+          <p>
+            Klik icon tambah pada kolom feedback lalu isi seluruh masukannya
+          </p>
         </div>
-        <div className="accordion-item" id='question3'>
-          <a className='accordion-link' href='#question3'>
-            Bagaimana cara menambah mata kuliah?
-          </a>
-          <div className="answer">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel possimus iusto aperiam, rerum nam tenetur, voluptatibus pariatur nulla commodi autem, necessitatibus deserunt optio fugiat sequi atque itaque? Dolorum, rem explicabo!
-            </p>
-          </div>
+      </div>
+      <div className="accordion-item" id="question3">
+        <a className="accordion-link" href="#question3">
+          Tidak ada feedback pada mata kuliah yang saya cari
+        </a>
+        <div className="answer">
+          <p>Belum ada yang memberikan feedback pada mata kuliah tersebut</p>
         </div>
+      </div>
     </div>
   )
 }
--- src/src/components/organism/HelpContent/index.tsx
+++ src/src/components/organism/HelpContent/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function HelpContent() {
   const steps = [
-    '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec feugiat metus, non pretium velit. Vivamus finibus nibh non vestibulum auctor.',
-    '2. Sed vulputate, quam in tempus porttitor, metus ligula placerat eros, eget pharetra ante purus et tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis vitae dui ut mauris pellentesque sodales.',
-    '3. Nunc iaculis egestas felis quis imperdiet. Fusce malesuada diam ac elementum feugiat.',
-    '4. Duis pretium turpis felis, eu placerat augue sagittis nec.',
+    '1. Cari mata kuliah yang ingin Anda lihat melalui fitur search pada halaman home',
+    '2. Terdapat filter keyword, jurusan, dan kategori untuk memudahkan pencarian',
+    '3. Pilih mata kuliah yang ingin Anda lihat',
+    '4. Anda dapat menambahkan feedback pada kolom feedback mata kuliah tersebut',
   ]
 
   return (
